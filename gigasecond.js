c628cf7caad3dc92b53767656a48c7260cc41900
Adam Sakhr <adam.sakhr@gmail.com>
2016-04-25 12:07:25 +0200

    LAST LAST Check

--- gigasecond.js
+++ gigasecond.js
@@ -4,13 +4,23 @@
 // Make sure to look at test.script.js--that should give you some hints about what is
 // expected here.
 
-function Gigasecond(birthday) {
-  this.birthday = new Date(birthday);
-}
+function Gigasecond(dateOfBirth) {
+  'use strict';
 
-Gigasecond.prototype.date = function() {
-  b = new Date(this.birthday.setSeconds(this.birthday.getSeconds() + 1000000000 ));
-  return new Date(b.getFullYear(), b.getMonth(), b.getDate());
+  this.dateOfBirth = dateOfBirth;
+
+  this.date = function() {
+  Gigasecond.prototype.date = function() {
+  var gs = Math.pow(10, 9);
+  var days = Math.ceil(((gs / 60) / 60) / 24);
+  var finalDate = this.birthday.setTime(this.dateOfBirthday.getTime() + days * 86400000);
+  var results = new Date(finalDate);
+  results.setSeconds(0);
+  results.setMinutes(0);
+  results.setHours(0);
+  return results;
+};
+  };
 };
 
-module.exports = Gigasecond;;
+module.exports = Gigasecond;
