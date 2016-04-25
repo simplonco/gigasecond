27d2e2513400a4d99524353c15a8e63aa2d81f14
Adam Sakhr <adam.sakhr@gmail.com>
2016-04-25 12:46:16 +0200

    Check1 File

--- gigasecond.js
+++ gigasecond.js
@@ -4,19 +4,14 @@
 // Make sure to look at test.script.js--that should give you some hints about what is
 // expected here.
 
-function Gigasecond(dateOfBirth) {
-  'use strict';
-
-  this.dateOfBirth = dateOfBirth;
-
-  this.date = function() {
-    var gigaBirthday = new Date(dateOfBirthday.getTime() + 1000000000000);
-          gigaBirthday.setSeconds(0);
-          gigaBirthday.setMinutes(0);
-          gigaBirthday.setHours(0);
-          return gigaBirthday;
-      };
-  };
-
+function Gigasecond(dateOfBirthday) {
+    this.dateOfBirthday = function() {
+        var gigaBirthday = new Date(dateOfBirthday.getTime() + 1000000000000);
+        gigaBirthday.setSeconds(0);
+        gigaBirthday.setMinutes(0);
+        gigaBirthday.setHours(0);
+        return gigaBirthday;
+    };
+}
 
 module.exports = Gigasecond;
